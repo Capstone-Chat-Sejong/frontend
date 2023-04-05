1e0ddf6810923e9d3e340c3e5df741f779d6cf32
mjmj0217 <mjmj0217@naver.com>
2023-04-05 17:01:34 +0900

    design: add global style, font

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./styles/font.css";
+import GlobalStyle from "./styles/GlobalStyle";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <GlobalStyle />
   </React.StrictMode>
 );
---
+++ src/styles/GlobalStyle.jsx
@@ -0,0 +1,46 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    overflow-wrap: break-word;
+    word-break: break-word;
+  }
+  *,
+  *::before,
+  *::after {  
+    font-family: S-CoreDream-3Light;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0; 
+  }
+  html, 
+  body {
+    height: 100%;
+  }
+  ul,
+  li {
+    list-style: none;
+  }
+  a,
+  a:hover,
+  a:active {
+    text-decoration: none;
+  }
+  button {
+    background-color: transparent;
+    cursor: pointer;
+    border: none;
+    outline: none;
+  }
+  input {
+    border: none;
+    outline: none;
+  }
+  textarea {
+    border: none;
+    outline: none;
+    resize: none;
+  }
+`;
+
+export default GlobalStyle;
