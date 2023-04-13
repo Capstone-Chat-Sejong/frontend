5cb737004be412bcceba978fe18ee3df3382900e
mjmj0217 <mjmj0217@naver.com>
2023-04-13 22:08:12 +0900

    design: change chat ballon shape

--- src/components/chatting/ChatBallon.jsx
+++ src/components/chatting/ChatBallon.jsx
@@ -3,19 +3,19 @@ import Serong from "../common/Serong";
 
 const Container = styled.div`
   display: flex;
-  align-items: center;
   justify-content: ${(props) => (props.isMine ? "flex-end" : "flex-start")};
   padding: 8px 10px;
   gap: 5px;
-
   font-size: 14px;
+  align-items: baseline;
 `;
 
 const Ballon = styled.div`
   background-color: ${(props) =>
     props.isMine ? `${props.theme.color.red}99` : props.theme.color.gray};
-  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.25);
-  border-radius: 60px;
+  box-shadow: 0px 0px 3px rgba(0, 0, 0, 0.25);
+  border-radius: ${(props) =>
+    props.isMine ? "50px 5px 50px 50px" : "5px 50px 50px 50px"};
   padding: 8px 15px;
   max-width: 70%;
   word-break: break-all;
