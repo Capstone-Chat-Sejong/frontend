d1ca8f94859d77040db5a6b32c6338f10fa05f1b
mjmj0217 <mjmj0217@naver.com>
2023-05-03 13:30:58 +0900

    feat: feedback button

--- src/components/chatting/ChatBallon.jsx
+++ src/components/chatting/ChatBallon.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import Serong from "../common/Serong";
+import { Icon } from "@iconify/react";
 
 const Container = styled.div`
   display: flex;
@@ -10,23 +11,55 @@ const Container = styled.div`
   align-items: baseline;
 `;
 
+const BallonBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 70%;
+`;
 const Ballon = styled.div`
   background-color: ${(props) =>
     props.isMine ? `${props.theme.color.red}99` : props.theme.color.gray};
   box-shadow: 0px 0px 3px rgba(0, 0, 0, 0.25);
   border-radius: ${(props) =>
-    props.isMine ? "50px 5px 50px 50px" : "5px 50px 50px 50px"};
-  padding: 8px 15px;
-  max-width: 70%;
+    props.isMine ? "35px 5px 35px 35px" : "5px 35px 35px 35px"};
+  padding: 10px 15px;
   word-break: break-all;
   font-size: 12px;
+`;
+
+const Feedback = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  gap: 6px;
+  padding: 0 5px;
 `;
 
 function ChatBallon({ isMine, content }) {
   return (
     <Container isMine={isMine}>
       {!isMine && <Serong size="45px" />}
-      <Ballon isMine={isMine}>{content}</Ballon>
+      <BallonBox>
+        <Ballon isMine={isMine}>
+          {content}
+          {!isMine &&
+            false && ( // TODO 피드백 받을 만한 정보일 때만, 클릭시 fill
+              <Feedback>
+                <Icon
+                  icon="bi:hand-thumbs-up"
+                  color="red"
+                  width="14"
+                  height="14"
+                />
+                <Icon
+                  icon="bi:hand-thumbs-down"
+                  color="blue"
+                  width="14"
+                  height="14"
+                />
+              </Feedback>
+            )}
+        </Ballon>
+      </BallonBox>
     </Container>
   );
 }
