ec47943a1403317cdef8074370c4ec62c63b7d26
mjmj0217 <mjmj0217@naver.com>
2023-06-02 01:39:52 +0900

    feat: scroll drag and drop

--- src/components/chatting/Slider.jsx
+++ src/components/chatting/Slider.jsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { useState, useRef } from "react";
+import { useMemo } from "react";
 
 const Slide = styled.div`
   background-color: ${({ theme }) => theme.color.gray};
@@ -18,16 +20,46 @@ const Container = styled.div`
   flex-wrap: nowrap;
   overflow-x: scroll;
   gap: 10px;
-`;
-function Slider({ content }) {
-  const result = [];
-  const contents = content.split("\n");
 
+  ::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+function Slider({ content }) {
+  const containerRef = useRef(null);
+  const contents = content.split("\n");
+  const [isDragging, setIsDragging] = useState(false);
+  const [startX, setStartX] = useState(0);
+
+  const result = [];
   for (let i = 0; i < contents.length; i += 8) {
     result.push(contents.slice(i, i + 8));
   }
+
+  // 가로 스크롤 드래그 앤 드롭
+  const handleMouseDown = (event) => {
+    setIsDragging(true);
+    setStartX(event.pageX + containerRef.current.scrollLeft);
+  };
+
+  const handleMouseMove = (event) => {
+    if (!isDragging) return;
+    containerRef.current.scrollLeft = startX - event.pageX;
+  };
+
+  const handleMouseUp = () => {
+    setIsDragging(false);
+  };
+
   return (
-    <Container>
+    <Container
+      ref={containerRef}
+      onMouseDown={handleMouseDown}
+      onMouseMove={isDragging ? handleMouseMove : null}
+      onMouseUp={handleMouseUp}
+      onMouseLeave={handleMouseUp}
+    >
       {result.map((v) => (
         <Slide>
           {v.map((k) => (
